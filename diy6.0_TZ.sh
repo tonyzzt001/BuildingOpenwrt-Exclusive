fac13870c517c77ce756d857909dca38cbbc2280
TTT <58526448+tonyzzt001@users.noreply.github.com>
2023-08-31 22:39:34 +0800

    Update diy6.0_TZ.sh

--- diy6.0_TZ.sh
+++ diy6.0_TZ.sh
@@ -34,7 +34,7 @@ svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/l
 # git clone https://github.com/firker/diy-ziyong.git package/diy-ziyong
 git clone https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
 # git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
-git clone https://github.com/immortalwrt/homeproxy.git package/luci-app-homeproxy
+# git clone https://github.com/immortalwrt/homeproxy.git package/luci-app-homeproxy
 git clone -b luci https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  package/luci-theme-argon-18.06
 # git clone -b 18.06 https://github.com/garypang13/luci-theme-edge.git package/luci-theme-edge
