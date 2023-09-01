85f84425dc65eedf4419f56464712a668bb629ab
TTT <58526448+tonyzzt001@users.noreply.github.com>
2023-09-01 20:30:22 +0800

    Update diy_TZ.sh

--- diy_TZ.sh
+++ diy_TZ.sh
@@ -26,15 +26,15 @@ sed -i 's/192.168.1.1/10.232.146.198/g' package/base-files/files/bin/config_gene
 sed -i 's/bootstrap/argon-18.06/g' ./feeds/luci/collections/luci/Makefile
 
 rm -rf ./feeds/luci/themes/luci-theme-argon
-# rm -rf ./feeds/luci/applications/luci-app-wrtbwmon
+rm -rf ./feeds/luci/applications/luci-app-wrtbwmon
 rm -rf ./feeds/packages/net/adguardhome
-# rm -rf ./feeds/packages/net/smartdns
-# rm -rf ./feeds/luci/applications/luci-app-mosdns
-# rm -rf ./feeds/packages/net/mosdns
+rm -rf ./feeds/packages/net/smartdns
+rm -rf ./feeds/luci/applications/luci-app-mosdns
+rm -rf ./feeds/packages/net/mosdns
 # rm -rf ./package/openwrt-passwall/v2ray-geodata
 # rm -rf ./package/ssr/v2ray-geodata
 # svn co https://github.com/garypang13/openwrt-packages/trunk/shadowsocksr-libev package/lean/shadowsocksr-libev
-svn co https://github.com/garypang13/openwrt-packages/trunk/luci-app-passwall feeds/passwall/luci-app-passwall
+# svn co https://github.com/garypang13/openwrt-packages/trunk/luci-app-passwall feeds/passwall/luci-app-passwall
 # svn co https://github.com/garypang13/openwrt-packages/trunk/luci-app-ssr-plus feeds/helloworld/luci-app-ssr-plus
 # svn co https://github.com/garypang13/openwrt-packages/trunk/smartdns feeds/packages/net/smartdns
 # svn co https://github.com/Lienol/openwrt-packages/trunk/net/https-dns-proxy feeds/packages/net/https-dns-proxy
