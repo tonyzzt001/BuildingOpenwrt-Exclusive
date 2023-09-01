da15cf1ceaa7c8d875e1720b0974e5d1ae984ae7
TTT <58526448+tonyzzt001@users.noreply.github.com>
2023-09-01 12:12:28 +0800

    Update project2.sh

--- project2.sh
+++ project2.sh
@@ -10,18 +10,18 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-rm -rf ./package/ctcgfw/luci-app-adguardhome
-rm -rf ./package/ctcgfw/luci-theme-argon-mod
-# rm -rf ./package/ctcgfw/luci-app-unblockneteasemusic
-# rm -rf ./package/ctcgfw/luci-app-unblockneteasemusic-mini
-rm -rf ./package/lean/luci-theme-argon
+# rm -rf ./package/ctcgfw/luci-app-adguardhome
+# rm -rf ./package/ctcgfw/luci-theme-argon-mod
+rm -rf ./package/ctcgfw/luci-app-unblockneteasemusic
+rm -rf ./package/ctcgfw/luci-app-unblockneteasemusic-mini
+# rm -rf ./package/lean/luci-theme-argon
 # rm -rf ./package/lean/luci-app-unblockneteasemusic-go
-rm -rf ./package/ntlf9t/AdGuardHome
-# rm -rf ./package/ntlf9t/luci-app-smartdns
-# rm -rf ./package/ntlf9t/smartdns
-# rm -rf ./package/ntlf9t/openwrt-udp2raw
-# rm -rf ./package/ntlf9t/dnscrypt-proxy-full
-# rm -rf ./feeds/packages/net/https-dns-proxy
+# rm -rf ./package/ntlf9t/AdGuardHome
+rm -rf ./package/ntlf9t/luci-app-smartdns
+rm -rf ./package/ntlf9t/smartdns
+rm -rf ./package/ntlf9t/openwrt-udp2raw
+rm -rf ./package/ntlf9t/dnscrypt-proxy-full
+rm -rf ./feeds/packages/net/https-dns-proxy
 # curl -fsSL  https://raw.githubusercontent.com/Lienol/openwrt-packages/dev-19.07/net/https-dns-proxy/files/https-dns-proxy.init > ./feeds/packages/net/https-dns-proxy/files/https-dns-proxy.init
 # wget https://raw.githubusercontent.com/Lienol/openwrt-packages/19.07/net/https-dns-proxy/files/https-dns-proxy.config -O ./feeds/packages/net/https-dns-proxy/files/https-dns-proxy.config
 # wget https://raw.githubusercontent.com/Lienol/openwrt-packages/19.07/net/https-dns-proxy/files/https-dns-proxy.init -O ./feeds/packages/net/https-dns-proxy/files/https-dns-proxy.init
