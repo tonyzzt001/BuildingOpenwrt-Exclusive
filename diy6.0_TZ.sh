29e326f618f8369007d245e2118bcd58d3143ad4
TTT <58526448+tonyzzt001@users.noreply.github.com>
2023-09-01 12:05:45 +0800

    Update diy6.0_TZ.sh

--- diy6.0_TZ.sh
+++ diy6.0_TZ.sh
@@ -23,7 +23,7 @@ sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=6.1/g' ./target/linux/x86/Makef
 # git clone https://github.com/jerrykuku/luci-app-vssr.git  package/luci-app-vssr
 # svn co https://github.com/messense/aliyundrive-webdav/trunk/openwrt package/aliyundrive-webdav
 svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
-# svn co https://github.com/linkease/istore/trunk/luci/luci-app-store package/luci-app-store
+svn co https://github.com/linkease/istore/trunk/luci/luci-app-store package/luci-app-store
 # svn co https://github.com/linkease/istore/trunk/luci/luci-app-store package/luci-app-store
 # svn co https://github.com/linkease/istore-ui/trunk/app-store-ui package/app-store-ui
 # git clone https://github.com/QiuSimons/openwrt-mos.git package/openwrt-mos
@@ -38,7 +38,7 @@ git clone https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
 git clone -b luci https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  package/luci-theme-argon-18.06
 # git clone -b 18.06 https://github.com/garypang13/luci-theme-edge.git package/luci-theme-edge
-git clone https://github.com/sirpdboy/luci-theme-opentopd.git package/luci-theme-opentopd
+# git clone https://github.com/sirpdboy/luci-theme-opentopd.git package/luci-theme-opentopd
 # git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
 # git clone https://github.com/xiaorouji/openwrt-passwall2.git package/luci-app-passwall2
 # git clone https://github.com/QiuSimons/openwrt-mos.git package/openwrt-mos
