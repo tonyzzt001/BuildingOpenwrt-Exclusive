cdf93d72a74215f7d59d9031f137dd1b1f192cad
TTT <58526448+tonyzzt001@users.noreply.github.com>
2023-09-02 10:21:46 +0800

    Update Project.sh

--- Project.sh
+++ Project.sh
@@ -36,6 +36,6 @@ svn co https://github.com/linkease/istore-ui/trunk/app-store-ui package/app-stor
 # svn co https://github.com/linkease/openwrt-themedog/trunk/luci/luci-theme-dog package/luci-theme-dog
 # svn co https://github.com/messense/aliyundrive-webdav/trunk/openwrt package/aliyundrive-webdav
 svn co https://github.com/kiddin9/openwrt-packages/trunk/adguardhome package/adguardhome
-svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-adguardhome package/luci-app-adguardhome
-svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
+# svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-adguardhome package/luci-app-adguardhome
+# svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
 svn co https://github.com/tonyzzt001/luci-app-aliddns openwrt/luci-app-aliddns
