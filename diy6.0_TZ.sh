240599d80520102b10b5dccd010f6595d2a3e740
TTT <58526448+tonyzzt001@users.noreply.github.com>
2023-09-02 13:01:13 +0800

    Update diy6.0_TZ.sh

--- diy6.0_TZ.sh
+++ diy6.0_TZ.sh
@@ -19,11 +19,11 @@ sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=6.1/g' ./target/linux/x86/Makef
 # sed -i '$a src-git jerryk https://github.com/jerrykuku/openwrt-package' feeds.conf.default
 # sed -i '$a src-git bypass https://github.com/garypang13/openwrt-bypass' feeds.conf.default
 # mv -vf ../mt7621_phicomm_k2p.dts ./target/linux/ramips/dts/mt7621_phicomm_k2p.dts
-#　git clone https://github.com/vernesong/OpenClash.git package/OpenClash
+# git clone https://github.com/vernesong/OpenClash.git package/OpenClash
 # git clone https://github.com/jerrykuku/luci-app-vssr.git  package/luci-app-vssr
 # svn co https://github.com/messense/aliyundrive-webdav/trunk/openwrt package/aliyundrive-webdav
 svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
-svn co https://github.com/linkease/istore/trunk/luci/luci-app-store package/luci-app-store
+# svn co https://github.com/linkease/istore/trunk/luci/luci-app-store package/luci-app-store
 # svn co https://github.com/linkease/istore/trunk/luci/luci-app-store package/luci-app-store
 # svn co https://github.com/linkease/istore-ui/trunk/app-store-ui package/app-store-ui
 # git clone https://github.com/QiuSimons/openwrt-mos.git package/openwrt-mos
@@ -31,13 +31,13 @@ svn co https://github.com/linkease/istore/trunk/luci/luci-app-store package/luci
 # git clone https://github.com/sbwml/openwrt-alist.git package/openwrt-alist
 # svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-bypass package/luci-app-bypass
 # git clone https://github.com/fw876/helloworld.git package/ssr
+# git clone https://github.com/firker/diy-ziyong.git package/diy-ziyong
 git clone https://github.com/tonyzzt001/luci-app-aliddns.git openwrt/luci-app-aliddns
-git clone https://github.com/firker/diy-ziyong.git package/diy-ziyong
 git clone https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
-# git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
-# git clone https://github.com/immortalwrt/homeproxy.git package/luci-app-homeproxy
 git clone -b luci https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  package/luci-theme-argon-18.06
+# git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
+# git clone https://github.com/immortalwrt/homeproxy.git package/luci-app-homeproxy
 # git clone -b 18.06 https://github.com/garypang13/luci-theme-edge.git package/luci-theme-edge
 # git clone https://github.com/sirpdboy/luci-theme-opentopd.git package/luci-theme-opentopd
 # git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
